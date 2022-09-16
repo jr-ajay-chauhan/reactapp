e8d469dad6e7919bed121117c07394758bcae2ae
ajay <aajjuu003@gmail.com>
2022-09-16 20:19:59 +0545

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './mycomponent/Header';
+import Footer from './mycomponent/Footer';
+import Todoitem from './mycomponent/Todoitem';
+import Todos from './mycomponent/Todos';
+
+function App() {
+  return (
+    <>
+ <Header/>
+ <Footer/>
+ <Todoitem/>
+ <Todos/>
+     </>
+  );
+}
+
+export default App;
